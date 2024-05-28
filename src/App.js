1d039d1b2ad7722dfe3cf8e90347b58faffec776
a4anthony <55565463+a4anthony@users.noreply.github.com>
2024-05-28 06:41:15 +0100

    travis deploy

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+          <span>
+              Anthony's React App
+          </span>
         {/*<p>*/}
         {/*  Touch Edit <code>src/App.js</code> and save to reload. lol by Anthony!!!!!*/}
         {/*</p>*/}
